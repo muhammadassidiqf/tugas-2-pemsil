2d71ce6489558b5da751b7856c8c964804b051f5
massidiqfattah <massidiqfattah@gmail.com>
2021-03-10 22:32:11 +0700

    final

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import os
 import random
 import locale
 
+st.title('Montecarlo')
+st.subheader('Nama : Muhammad Assidiq Fattah')
+st.subheader('NRP : 152017015')
+st.sidebar.subheader('Probabilitas')
+
 def find_sum(d):
     res = 0
 
@@ -19,11 +24,6 @@ def rupiah_format(angka, with_prefix=False, desimal=2):
     if with_prefix:
         return "Rp. {}".format(rupiah)
     return rupiah
-# def find_average(e):
-#     res = 0
-
-#     for i in range(len(e)):
-#         res = 
 
 uploaded_file = st.file_uploader("Pilih file csv yang akan digunakan")
 if uploaded_file is not None:
@@ -58,5 +58,50 @@ if uploaded_file is not None:
         st.sidebar.text("BB Interval "+str(i)+": "+str(round(ab,5)))
         ab = aa + 0.001
 
+    des = 229900
+    st.text("Harga Sepatu: Rp."+str(rupiah_format(des)))
+    col1, col2, col3 = st.beta_columns(3)
+    randomlist = []
+    for i in range(1,16):
+        n = random.random()
+        col1.text("Angka acak "+str(i)+": "+str(round(n,3)))
+        randomlist.append(n)
+        
+    a = 1
+    perlist = []
+    for i in range(len(randomlist)):
+        if (randomlist[i] <= reslist[0]):
+            b = 0
+            col2.text("Permintaan "+str(a)+": "+str(b))
+        elif (randomlist[i] <= reslist[1]):
+            b = 1
+            col2.text("Permintaan "+str(a)+": "+str(b))
+        elif (randomlist[i] <= reslist[2]):
+            b = 2 
+            col2.text("Permintaan "+str(a)+": "+str(b))
+        elif (randomlist[i] <= reslist[3]):
+            b = 3
+            col2.text("Permintaan "+str(a)+": "+str(b))
+        elif (randomlist[i] <= reslist[4]):
+            b = 4 
+            col2.text("Permintaan "+str(a)+": "+str(b))
+        perlist.append(b)
+        a = a+1
+
+    c = 1
+    pemlist = []
+    for i in range(len(perlist)):
+        d = perlist[i]*int(des)
+        col3.text("Pemasukan "+str(c)+": Rp."+str(rupiah_format(d)))
+        pemlist.append(d)
+        c = c+1
+    
+    per = int(find_sum(perlist))
+    st.text("")  
+    st.text("Jumlah Permintaan : "+str(per))
+    aver_per = int(find_sum(perlist))/len(perlist) 
+    st.text("Rata-rata Permintaan Per Hari: "+str(aver_per))
+    aver_pem = int(find_sum(pemlist))/len(pemlist) 
+    st.text("Rata-rata Pemasukan Per Hari: Rp."+str(rupiah_format(aver_pem)))
 else:
     st.write("Silahkan Pilih file csv yang akan digunakan !")
