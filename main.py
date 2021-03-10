d0512c0d482dc9f9f141391e9003ee4b6a8bfe60
massidiqfattah <massidiqfattah@gmail.com>
2021-03-10 13:54:32 +0700

    batas

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas
 import streamlit as st
 import pandas as pd 
 import os
+import random
+import locale
 
 def find_sum(d):
     res = 0
@@ -10,6 +12,18 @@ def find_sum(d):
         res += d[i]
     
     return res
+
+def rupiah_format(angka, with_prefix=False, desimal=2):
+    locale.setlocale(locale.LC_NUMERIC, 'IND')
+    rupiah = locale.format("%.*f", (desimal, angka), True)
+    if with_prefix:
+        return "Rp. {}".format(rupiah)
+    return rupiah
+# def find_average(e):
+#     res = 0
+
+#     for i in range(len(e)):
+#         res = 
 
 uploaded_file = st.file_uploader("Pilih file csv yang akan digunakan")
 if uploaded_file is not None:
@@ -27,6 +41,22 @@ if uploaded_file is not None:
         prob = y[i] / frek
         st.sidebar.text("Probabilitas "+str(i)+": "+str(round(prob,3)))
     st.sidebar.text("")  
+    
+    res = 0
+    reslist = []
+    for i in range(len(y)):   
+        res += y[i] / frek
+        st.sidebar.text("Probabilitas Komulatif "+str(i)+": "+str(round(res,3)))
+        reslist.append(res)
+    st.sidebar.text("")  
+    
+    # st.text(reslist[1])
+    aa = 0
+    ab = 0
+    for i in range(len(y)):
+        aa += y[i] / frek
+        st.sidebar.text("BB Interval "+str(i)+": "+str(round(ab,5)))
+        ab = aa + 0.001
 
 else:
     st.write("Silahkan Pilih file csv yang akan digunakan !")
